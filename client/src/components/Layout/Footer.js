b40c1f75bb63c204a7632ae58163d86fdb590733
Mrdasakasagar31 <creatermrdas31200203@gmail.com>
2024-10-30 23:51:43 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
-    <div className='footer' style={{ marginBottom: 0, paddingBottom: 0 }}>
+    <div className='footer'>
       <h3 className='text-center'>All Right Reserved &copy; SAGAR KUMAR DAS</h3>
       <p className='text-center mt-3'>
         <Link className='down' to="/about"> About</Link> |
         <Link className='down' to="/contact"> Contact</Link> |
         <Link className='down' to="/policy"> Policy</Link>
       </p>
+    <p>
+    </p?
     </div>
   );
 };
