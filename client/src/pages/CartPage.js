9bffc1acd42450a0988cbba28810b37c05bae169
Mrdasakasagar31 <creatermrdas31200203@gmail.com>
2024-10-30 19:39:13 +0530

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -66,7 +66,7 @@ const CartPage = () => {
     //get payment gateway token
     const getToken = async () => {
         try {
-            const { data } = await axios.get("https://ecomfinal.onrender.com/api/v1/product/braintree/token");
+            const { data } = await axios.get("https://ecom-final-fixed-backup.onrender.com/api/v1/product/braintree/token");
             setClientToken(data?.clientToken);
         } catch (error) {
             console.log(error);
@@ -83,7 +83,7 @@ const CartPage = () => {
         try {
             setLoading(true);
             const { nonce } = await instance.requestPaymentMethod();
-            const { data } = await axios.post("https://ecomfinal.onrender.com/api/v1/product/braintree/payment", {
+            const { data } = await axios.post("https://ecom-final-fixed-backup.onrender.com/api/v1/product/braintree/payment", {
                 nonce,
                 cart,
             });
