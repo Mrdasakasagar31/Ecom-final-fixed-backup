22883c4068e3414217c2781ed7ecc0a7c1fd8ffc
Mrdasakasagar31 <creatermrdas31200203@gmail.com>
2024-10-30 19:37:37 +0530

    Update auth.js

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,48 +1,3 @@
-// import { useState, useEffect, useContext, createContext } from "react";
-// import axios from "axios";
-
-// const AuthContext = createContext()
-
-// const AuthProvider = ({ children }) => {
-//     const [auth, setAuth] = useState({
-//         user: null,
-//         token: ""
-//     });
-
-//     //default axios
-// <<<<<<< HEAD
-//     axios.defaults.baseURL = 'https://ecomfinal.onrender.com';
-// =======
-//      axios.defaults.baseURL = 'https://ecomfinal.onrender.com';
-// >>>>>>> 99041aa28e8beb0f65f6285d0d6536fef8d9a7f0
-//     axios.defaults.headers.common['Authorization'] = auth?.token
-
-//     useEffect(() => {
-//         const data = localStorage.getItem("auth");
-//         if (data) {
-//             const parseData = JSON.parse(data);
-//             setAuth({
-//                 ...auth,
-//                 user: parseData.user,
-//                 token: parseData.token,
-//             });
-//         }
-
-//         // eslint-disable-next-line
-//     }, []);
-//     return (
-//         <AuthContext.Provider value={[auth, setAuth]}>
-//             {children}
-//         </AuthContext.Provider>
-//     )
-// }
-
-// // custom Hook
-// const useAuth = () => useContext(AuthContext)
-// export { useAuth, AuthProvider }
-// <<<<<<< HEAD
-
-
 // =======
 // >>>>>>> 99041aa28e8beb0f65f6285d0d6536fef8d9a7f0
 
@@ -58,7 +13,7 @@ const AuthProvider = ({ children }) => {
     });
 
     // Set default axios base URL and headers
-    axios.defaults.baseURL = 'https://ecomfinal.onrender.com';
+    axios.defaults.baseURL = 'https://ecom-final-fixed-backup.onrender.com';
     axios.defaults.headers.common['Authorization'] = auth?.token;
 
     useEffect(() => {
